0906deedbc70e5a1eb2410bbb319e4edf42a2619
Gabriel <gabrielferreiradsl@hotmail.com>
2022-07-02 16:15:07 -0300

    fix: actions/ecommerce -> correção da action que adiciona o filtro

--- front-end/src/redux/actions/ecommerceActions.ts
+++ front-end/src/redux/actions/ecommerceActions.ts
@@ -1,7 +1,17 @@
+import { Dispatch } from "redux";
 import { IResponseAPI } from "../../interfaces/IResponseAPI";
 
 export const SET_FILTERED_PRODUCTS = 'SET_FILTERED_PRODUCTS';
 export type SetFilteredProductsAction = {
   type: typeof SET_FILTERED_PRODUCTS;
   payload: IResponseAPI;
+};
+
+export const setFilteredProducts = (payload: IResponseAPI) => (
+  dispatch: Dispatch<SetFilteredProductsAction>
+) => {
+  dispatch({
+    type: SET_FILTERED_PRODUCTS,
+    payload,
+  });
 };
