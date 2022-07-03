1b357178bb219cd1c39f0337092d3860e38d32aa
Gabriel <gabrielferreiradsl@hotmail.com>
2022-07-03 11:50:17 -0300

    feat: recupera carrinho do localStorage

---
+++ front-end/src/components/Header.tsx
@@ -0,0 +1,77 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { getLocalStorage } from "../helpers/LSHelpers";
+import { ICart } from "../interfaces/ICart";
+import {
+  setInitialCartFromLS,
+} from "../redux/actions/ecommerceActions";
+import { useAppDispatch } from "../redux/AppHooks";
+import HeaderLinks from "./HeaderLinks";
+
+import profileLogo from "../../public/profileLogo.png";
+import searchLogo from "../../public/searchLogo.png";
+import buttonSearchLogo from "../../public/buttonSearch.png";
+import HeaderCart from "./HeaderCart";
+
+const Header: NextPage = () => {
+  const [textSearch, setTextSearch] = useState<string>("");
+  const [searchbar, setSearchbar] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const cartLS: ICart[] = getLocalStorage("products");
+    if (cartLS) {
+      dispatch(setInitialCartFromLS(cartLS));
+    }
+  }, [dispatch]);
+
+  return (
+    <header>
+      <div className="header-main">
+        <div>
+          <Image
+            className="logo"
+            src="https://img.wine.com.br/logo/wine/black/wine.svg"
+            alt="wine"
+            width="100px"
+            height="45px"
+          />
+        </div>
+        <HeaderLinks />
+        <div className="header-interactive">
+          <Image
+            className="searchbar"
+            role="button"
+            onClick={() => setSearchbar(!searchbar)}
+            src={searchLogo}
+            alt="wine"
+            width="53px"
+            height="53px"
+          />
+          <Image src={profileLogo} alt="wine" width="53px" height="50px" />
+          <HeaderCart />
+        </div>
+      </div>
+      {searchbar && (
+        <div>
+          <input
+            placeholder="Pesquisar"
+            type="text"
+            onChange={(ev) => setTextSearch(ev.target.value)}
+          />
+          <Image
+            role="button"
+            onClick={() => ''}
+            src={buttonSearchLogo}
+            width="32px"
+            height="5px"
+            alt="search"
+          />
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;
