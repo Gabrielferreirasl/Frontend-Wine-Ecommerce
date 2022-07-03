1e0533b3649d9c085a966942293d6cbf7b9e85f3
Gabriel <gabrielferreiradsl@hotmail.com>
2022-07-03 16:16:56 -0300

    feat: adiciona ao carrinho ao clicar no btn

--- front-end/src/pages/products/[page].tsx
+++ front-end/src/pages/products/[page].tsx
@@ -1,13 +1,43 @@
 import { GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
 import Filters from "../../components/Filters";
 import Header from "../../components/Header";
 import { IResponseAPI } from "../../interfaces/IResponseAPI";
+import { useAppSelector } from "../../redux/AppHooks";
 
-const Page = () => {
+import ProductCard from "../../components/ProductCard";
+import { MainPageStyle } from "../../../styles/page/styles";
+
+const Page = ({ data }: { data: IResponseAPI }) => {
+  const { isFallback } = useRouter();
+  const filter = useAppSelector((state) => state.ecommerce.filter);
+  const infoData: IResponseAPI = filter || data;
+
   return (
     <>
       <Header />
-      <Filters />
+      <div style={{ display: "flex" }}>
+        <Filters />
+        <MainPageStyle>
+          {isFallback ? (
+            <h2 className="loading">Carregando...</h2>
+          ) : (
+            <>
+              <div className="found-products">
+                <p>
+                  <span>{infoData.totalItems}</span> produtos encontrados
+                </p>
+              </div>
+              <div className="all-products-container">
+                {infoData.items.map((product) => (
+                  <ProductCard key={product.id} product={product} />
+                ))}
+              </div>
+            </>
+          )}
+        </MainPageStyle>
+      </div>
     </>
   );
 };
@@ -22,13 +52,13 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: true
+    fallback: true,
   };
-}
+};
 
-export const getStaticProps: GetStaticProps = async (context) => { 
+export const getStaticProps: GetStaticProps = async (context) => {
   const { page } = context.params;
-  const BASE_URL = 'https://wine-back-test.herokuapp.com/'
+  const BASE_URL = "https://wine-back-test.herokuapp.com/";
 
   const response = await fetch(`${BASE_URL}products?page=${page}&limit=9`);
   const data: IResponseAPI = await response.json();
@@ -39,6 +69,6 @@ export const getStaticProps: GetStaticProps = async (context) => {
     },
     revalidate: 10,
   };
-}
+};
 
 export default Page;
