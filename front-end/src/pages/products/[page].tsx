91ba95d5dfbfc57a8ba1379a049480037555d8b5
Gabriel <gabrielferreiradsl@hotmail.com>
2022-07-03 12:44:33 -0300

    feat: getStaticProps na page

--- front-end/src/pages/products/[page].tsx
+++ front-end/src/pages/products/[page].tsx
@@ -1,5 +1,7 @@
+import { GetStaticProps } from "next";
 import Filters from "../../components/Filters";
 import Header from "../../components/Header";
+import { IResponseAPI } from "../../interfaces/IResponseAPI";
 
 const Page = () => {
   return (
@@ -9,5 +11,20 @@ const Page = () => {
     </>
   );
 };
+
+export const getStaticProps: GetStaticProps = async (context) => { 
+  const { page } = context.params;
+  const BASE_URL = 'https://wine-back-test.herokuapp.com/'
+
+  const response = await fetch(`${BASE_URL}products?page=${page}&limit=9`);
+  const data: IResponseAPI = await response.json();
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 10,
+  };
+}
 
 export default Page;
