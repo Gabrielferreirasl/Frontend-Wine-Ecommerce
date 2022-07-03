5a761979bbb126622aa269c77984ed4b56d43fd0
Gabriel <gabrielferreiradsl@hotmail.com>
2022-07-03 20:06:59 -0300

    feat: logo do cart redireciona para a pagina do carrinho

--- front-end/src/components/HeaderCart.tsx
+++ front-end/src/components/HeaderCart.tsx
@@ -1,21 +1,30 @@
 import Image from "next/image";
 import { useAppSelector } from "../redux/AppHooks";
 import cartLogo from "../../public/cartLogo.png";
+import Link from "next/link";
 
 const HeaderCart = () => {
   const cart = useAppSelector((store) => store.ecommerce.cart);
 
   const getTotalItems = () => {
     if (cart.length === 0) return cart.length;
-    return cart.reduce((total, p) => total + p.quantity,0)
-  }
+    return cart.reduce((total, p) => total + p.quantity, 0);
+  };
 
   return (
     <div className="cart">
-      <Image src={cartLogo} alt="wine" width="55px" height="55px" />
+      <Link href="/cart">
+        <Image
+          style={{ cursor: "pointer" }}
+          src={cartLogo}
+          alt="wine"
+          width="55px"
+          height="55px"
+        />
+      </Link>
       <span>{getTotalItems()}</span>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
 export default HeaderCart;
