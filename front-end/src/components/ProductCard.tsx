7f73cb9784702c77ff920e152f65c444ac88d087
Gabriel <gabrielferreiradsl@hotmail.com>
2022-07-03 16:00:31 -0300

    feat: component ProductCard

---
+++ front-end/src/components/ProductCard.tsx
@@ -0,0 +1,45 @@
+import { IProduct } from "../interfaces/IProduct";
+import Image from "next/image";
+import { useAppDispatch } from "../redux/AppHooks";
+import Link from "next/link";
+import PageProductDetails from "./PageProductDetails";
+
+const ProductCard = ({ product }: { product: IProduct }) => {
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <div className="product-container">
+        <Link
+          href={{
+            pathname: `/products/details/${product.name}-${product.id}`,
+            query: { product: JSON.stringify(product) },
+          }}
+          rel="noopener noreferrer"
+          as={`/products/details/${product.name}-${product.id}`}
+        >
+          <div className="product-info">
+            <div className="container-image">
+              <Image
+                src={product.image}
+                alt="wine"
+                width="115px"
+                height="176px"
+              />
+            </div>
+            <PageProductDetails product={product} />
+          </div>
+        </Link>
+      </div>
+      <div className="container-addBtn">
+        <button
+          type="button"
+        >
+          ADICIONAR
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
